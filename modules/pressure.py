48ad54bd38d82e322627a14b6dbd0c29e9abd9ba
jameskwonlee <james@fevr.io>
2024-03-27 12:01:13 -0700

    add potentiation logic to touch sensors

--- modules/pressure.py
+++ modules/pressure.py
@@ -1,12 +1,18 @@
 #Start by importing all necessary libraries and packages 
 import RPi.GPIO as GPIO
 import time
+import smbus
+
+address = 0x48
+A0 = 0x40
+
+bus = smbus.SMBus(1)
 
 #Set the GPIO to BCM Mode
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
 #Set Pin 4 to be our Sniffer Pin, We want this to be an Input so we set it as such
-GPIO.setup(4,GPIO.IN)
+# GPIO.setup(4,GPIO.IN)
 
 #This variable will be used to determine if pressure is being applied or not
 prev_input = 0
@@ -14,18 +20,23 @@ prev_input = 0
 #Create a Loop that goes on as long as the script is running
 while True:
 
-    #take a reading from the pressure pad (based on the voltage able to get to pin 4)
-    input = GPIO.input(4)
+    bus.write_byte(address, A0)
 
-    #if the last reading was low and this one high the pressure pad is being pressed!
-    if ((not prev_input) and input):
+    current_value = bus.read_byte(address)
 
-    #Print that fact to the shell, RIP David Bowie
-        print("Under Pressure")
+    # calibrate potentiometer to 49
+    original_value = 49
+    package_count = 0
 
-    #update previous input so we can avoid spamming the Shell with messages, 
-    #this section of the script is also a perfect place to add threshold values to active other devices 
-    prev_input = input
+    while (current_value > original_value):
+        if current_value > original_value:
+            package_count += 1
+            original_value = current_value
+
+    
+
+    print(f"Current Value: {current_value}")
+    print(f"Package Count: {package_count}")
 
     #Have a slight pause here, also to avoid spamming the shell with data
     time.sleep(0.10)
